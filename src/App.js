6eb78c4b79f4aa84424c64ac867be0151c9cad1a
CoccoRandela <rocco.candela94@gmail.com>
2022-09-23 10:39:19 +0100

    Make basic homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Content, Navbar } from "./components";
+import "./App.css";
+
+export default function App() {
+    return (
+        <>
+            <header>
+                <h2>BetterLate</h2>
+            </header>
+            <Navbar/>
+            <Content/>
+        </> 
+    )    
+}
