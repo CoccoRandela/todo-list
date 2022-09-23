eea0806af77a9d27289ae4b625a176ca00b005a5
CoccoRandela <rocco.candela94@gmail.com>
2022-09-23 09:03:48 +0100

    Create basic navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <button className="nav-btn projects-btn">Projects</button>
+            <button className="nav-btn calendar-btn">Calendar</button>
+        </nav>
+    )
+}
