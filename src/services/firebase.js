4f7ef53038a56cb989160e3c1a58f1c55c826c7c
CoccoRandela <rocco.candela94@gmail.com>
2022-10-19 15:09:06 +0100

    Remove emulators

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -21,7 +21,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-connectAuthEmulator(auth, 'http://localhost:9099')
 export const db = getFirestore();
-connectFirestoreEmulator(db, 'localhost', 3001);
 const analytics = getAnalytics(app);
