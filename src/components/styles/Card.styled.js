ed2f4cf4cf84fdc3564a7e519e84f65125b1211f
CoccoRandela <rocco.candela94@gmail.com>
2022-10-19 16:31:36 +0100

    fix card style for small screens

--- src/components/styles/Card.styled.js
+++ src/components/styles/Card.styled.js
@@ -5,14 +5,15 @@ export const CardStyles = styled.div`
     display: grid;
     grid-auto-flow: column;
     grid-template-rows: repeat(${({r}) => r}, minmax(min-content, 1em));
-    grid-template-columns: 5fr 1fr;
+    grid-template-columns: minmax(100px, 80%) 30%;
+    max-width: 100%;
     row-gap: .5em;
-    column-gap: .5em;
     background-color: ${({c, theme}) => c ? c : theme.colors.dark};
     padding: 1em;
     border-radius: 1em;
 
     input {
+        max-width: fit-content;
         background-color: transparent;
         color: ${({theme}) => theme.colors.light};
         border: none;
